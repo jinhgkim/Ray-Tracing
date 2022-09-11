7123eddba00586a29e0ba44f4d40752a40ed4642
Jinnie Kim <jinhgkim@gmail.com>
2022-09-10 21:53:16 -0700

    anti aliasing

--- Ray Tracing/math.h
+++ Ray Tracing/math.h
@@ -1,9 +1,10 @@
 #ifndef MATH_H
 #define MATH_H
 
-#include<memory>
-#include<cmath>
-#include<limits>
+#include <memory>
+#include <cmath>
+#include <limits>
+#include <cstdlib>
 
 // Usings
 using std::shared_ptr;
@@ -15,14 +16,29 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
-
 inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180.0;
 }
 
 // Common Headers
-
 #include "ray.h"
 #include "vec3.h"
+
+// Random number generator
+inline double random_double() {
+	// random real in [0,1)
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+	// random real in [min,max)
+	return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
+}
 
 #endif // !MATH_H
